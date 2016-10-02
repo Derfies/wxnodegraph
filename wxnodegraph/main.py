e20cd301fd9df7ddf5410793d546b24a3227a42f
derfies <derfies@git>
2016-10-03 10:49:39 +1100

    Clean Up
    - Cleaned up a lot of code. Pushing for better object oriented design.
    - All wires now using the one class (even the temp wire)
    - Now using Graphics Context which provides nice antialiasing.
    - Improved the general look of the widget.
    - Fixed issues with redrawing rects. All drawing artifacts appear fixed.

--- wxnodegraph/main.py
+++ wxnodegraph/main.py
@@ -2,46 +2,68 @@ import math
 
 import wx
 
+#import bezier
+
 
 W = 2000
 H = 2000
-hitradius = 5
+HIT_RADIUS = 5
 
-
-PLUG_RADIUS = 10
+PLUG_MARGIN = 6
+PLUG_RADIUS = 4
+PLUG_HIT_RADIUS = 10
 PLUG_SPACING = 20
 PLUG_TYPE_IN = 0
-PLUG_TYPE_OUT = 0
+PLUG_TYPE_OUT = 1
+
+ROUND_CORNER_RADIUS = 8
+
+TITLE_INSET_X = 5
+TITLE_INSET_Y = 1
 
 
-class PdcRect( object ):
-    pass
+WIRE_COLOUR = 'black'
+WIRE_THICKNESS = 2
 
 
 class Wire( object ):
     
-    def __init__( self, node1, plug1, node2, plug2 ):
-        self.node1 = node1
-        self.plug1 = plug1
-        self.node2 = node2
-        self.plug2 = plug2
+    def __init__( self, pnt1, pnt2 ):
+        self.pnt1 = pnt1
+        self.pnt2 = pnt2
         self._idx = wx.NewId()
+
+        # HAXXOR
+        self.pen = wx.Pen( WIRE_COLOUR, WIRE_THICKNESS )
 
     def Draw( self, dc ):
         pnts = []
-
-        #pos = wx.Point( self.pos[0], self.pos[1] )
-        #nPos = wx.Point( self.node.pos[0], self.node.pos[1] )
-
-        pnt1 = self.node1.pos + self.plug1.pos
-        pnt2 = self.node1.pos + self.plug1.pos
-
-        pnts.append( pnt1 )
-        pnts.append( pnt1 + wx.Point( 20, 0 ) )
-        pnts.append( pnt2 - wx.Point( -20, 0 ) )
-        pnts.append( pnt2 )
+        pnts.append( self.pnt1 )
+        pnts.append( self.pnt1 + wx.Point( 10, 0 ) )
+        pnts.append( self.pnt2 - wx.Point( 10, 0 ) )
+        pnts.append( self.pnt2 )
+        dc.SetPen( self.pen )
         dc.DrawSpline( pnts )
+        '''
+        # HAXXOR - New bezier curve.
+        newPnts = []
+        for point in bezier.bezier_curve_range( 40, pnts ):
+            newPnts.append( point )
 
+        #dc.DrawSpline( newPnts )
+        lastPnt = newPnts[0]
+        for pnt in newPnts:
+            dc.DrawLine( lastPnt[0], lastPnt[1], pnt[0], pnt[1]) 
+            lastPnt = pnt
+        '''
+
+    def GetRect( self ):
+        minX = min( self.pnt1[0], self.pnt2[0] )
+        minY = min( self.pnt1[1], self.pnt2[1] )
+        size = self.pnt2 - self.pnt1
+        rect = wx.Rect( minX - 10, minY, abs( size[0] ) + 20, abs( size[1] ) )
+        return rect.Inflate( self.pen.GetWidth(), self.pen.GetWidth() )
+        
 
 class Plug( object ):
     
@@ -51,41 +73,47 @@ class Plug( object ):
         self.type = type_
         self.node = node
 
+        self.wires = []
+
     def Draw( self, dc ):
-        #pos = wx.Point( self.pos[0], self.pos[1] )
-       # nPos = wx.Point( self.node.pos[0], self.node.pos[1] )
         final = self.pos + self.node.pos
+        dc.SetPen( wx.Pen( 'grey',style=wx.TRANSPARENT ) )
+        dc.SetBrush( wx.Brush( 'black', wx.SOLID ) )
         dc.DrawCircle( final.x, final.y, self.radius )
 
     def HitTest( self, pos ):
         pnt = pos - self.pos
         dist = math.sqrt( math.pow( pnt.x, 2 ) + math.pow( pnt.y, 2 ) )
-        if math.fabs( dist ) < self.radius:
+        if math.fabs( dist ) < PLUG_HIT_RADIUS:
             return True
+
+    def CanConnect( self ):
+        pass
+
+    def Disconnect( self ):
+        pass
 
 
 class Node( object ):
 
     def __init__( self, label, pos, ins, outs ):
         self.label = label
-        #self.pos = pos
         self.pos = wx.Point( pos[0], pos[1] )
 
         self._idx = wx.NewId()
         self.size = (100, 100)
         self.ins = ins
         self.outs = outs
-        self.idx = wx.NewId()
         self.selected = False
         self.drawn = False
 
         # HAXXOR
         self.plugs = []
         for i, in_ in enumerate( self.ins ):
-            plug = Plug( (PLUG_RADIUS, 20 + PLUG_SPACING * i), PLUG_RADIUS, PLUG_TYPE_IN, self )
+            plug = Plug( (PLUG_MARGIN, 40 + PLUG_SPACING * i), PLUG_RADIUS, PLUG_TYPE_IN, self )
             self.plugs.append( plug )
         for i, out in enumerate( self.outs ):
-            plug = Plug( (self.size[0] - PLUG_RADIUS - 1, 20 + PLUG_SPACING * i), PLUG_RADIUS, PLUG_TYPE_OUT, self )
+            plug = Plug( (self.size[0] - PLUG_MARGIN - 1, 40 + PLUG_SPACING * i), PLUG_RADIUS, PLUG_TYPE_OUT, self )
             self.plugs.append( plug )
 
         self.wires = []
@@ -93,8 +121,16 @@ class Node( object ):
     def Draw( self, dc ):
         x, y = self.pos
         w, h = self.size
-        dc.DrawRoundedRectangle( x, y, w, h, 5 )
-        dc.DrawText( self.label, x, y )
+        dc.SetPen( wx.Pen( 'black', 1 ) )
+        dc.SetBrush( wx.Brush( 'white', wx.SOLID ) )
+        dc.DrawRoundedRectangle( x, y, w, h, ROUND_CORNER_RADIUS )
+        #dc.SetFont( self.GetFont().SetWeight( wx.BOLD ) )
+
+        newFont = wx.SystemSettings_GetFont(wx.SYS_DEFAULT_GUI_FONT)
+        newFont.SetWeight(wx.BOLD)
+        dc.SetFont( newFont )
+
+        dc.DrawText( self.label, x + TITLE_INSET_X, y + TITLE_INSET_Y )
 
         # Draw ins / outs.
         for plug in self.plugs:
@@ -109,23 +145,23 @@ class Node( object ):
         self.pos = rect.GetPosition()
         self.size = rect.GetSize()
 
+    def GetRect( self ):
+        return wx.Rect( self.pos[0], self.pos[1], self.size[0], self.size[1] )
+
 
 class NodeGraph( wx.ScrolledWindow ):
     def __init__(self, parent, id, log, size = wx.DefaultSize):
         wx.ScrolledWindow.__init__(self, parent, id, (0, 0), size=size, style=wx.SUNKEN_BORDER)
 
         self.nodes = {}
-        self._startPos = None
-        self._pos = None
-
         self.srcNode = None
         self.srcPlug = None
+        self.tmpWire = None
 
         self.maxWidth  = W
         self.maxHeight = H
-        self.x = self.y = 0
 
-        self.SetBackgroundColour( 'WHITE' )
+        self.SetBackgroundColour( 'Grey' )
         self.SetVirtualSize( (self.maxWidth, self.maxHeight) )
         self.SetScrollRate( 20, 20 )
         
@@ -133,106 +169,113 @@ class NodeGraph( wx.ScrolledWindow ):
         self.pdc = wx.PseudoDC()
         self.Bind( wx.EVT_PAINT, self.OnPaint )
         self.Bind( wx.EVT_ERASE_BACKGROUND, lambda x: None )
-        self.Bind( wx.EVT_MOUSE_EVENTS, self.OnMouse )
-        
-        # vars for handling mouse clicks
-        self.dragid = -1
-        self.lastpos = (0,0)
+        self.Bind( wx.EVT_LEFT_DOWN, self.OnLeftDown )
+        self.Bind( wx.EVT_MOTION, self.OnMotion )
+        self.Bind( wx.EVT_LEFT_UP, self.OnLeftUp )
 
-    def AddNode( self, label, pos, ins, outs ):
-        node = Node( label, pos, ins, outs )
-        self.nodes[node._idx] = node
-        self.DoDrawing( self.pdc )
-        return node
-    
-    def ConvertEventCoords(self, event):
+    def ConvertCoords( self, pnt ):
         xView, yView = self.GetViewStart()
         xDelta, yDelta = self.GetScrollPixelsPerUnit()
-        return (event.GetX() + (xView * xDelta),
-            event.GetY() + (yView * yDelta))
+        return wx.Point(pnt[0] + (xView * xDelta), pnt[1] + (yView * yDelta))
 
     def OffsetRect(self, r):
         xView, yView = self.GetViewStart()
         xDelta, yDelta = self.GetScrollPixelsPerUnit()
         r.OffsetXY(-(xView*xDelta),-(yView*yDelta))
 
-    def OnMouse(self, event):
-        x, y = self.ConvertEventCoords( event )
-        winSpacePos = wx.Point( x, y )
+    def AddNode( self, label, pos, ins, outs ):
+        node = Node( label, pos, ins, outs )
+        self.pdc.SetId( node._idx )
+        node.Draw( self.pdc )
+        self.pdc.SetIdBounds( node._idx, node.GetRect() )
+        self.nodes[node._idx] = node
+        return node
 
-        if event.LeftDown():
-            #x, y = self.ConvertEventCoords( event )
-            
-            idxs = self.pdc.FindObjects( x, y, hitradius )
-            if idxs:
-                self.dragid = idxs[0]
-                self.lastpos = (event.GetX(),event.GetY())
-
-            if self.dragid in self.nodes:
-                node = self.nodes[self.dragid]
-                plug = node.HitTest( winSpacePos )
-                if plug is not None:
-                    self._startPos = x, y
-                    self.srcNode = node
-                    self.srcPlug = plug
-
-        elif event.Dragging() or event.LeftUp():
-            if self.dragid != -1 and self._startPos is None:
-                x,y = self.lastpos
-                dx = event.GetX() - x
-                dy = event.GetY() - y
-                r = self.pdc.GetIdBounds(self.dragid)
-                self.pdc.TranslateId(self.dragid, dx, dy)
-                r2 = self.pdc.GetIdBounds(self.dragid)
-                r = r.Union(r2)
-                #r.Inflate(4,4)
-                self.OffsetRect(r)
-                self.RefreshRect(r, False)
-                self.lastpos = (event.GetX(),event.GetY())
-                self.nodes[self.dragid].UpdateRect( r )
-            if event.LeftUp():
-                self.dragid = -1
-        
-        if event.LeftUp():
-            
-            # Attempt to make a connection.
+    def OnLeftDown( self, evt ):
+        pnt = evt.GetPosition()
+        winPnt = self.ConvertCoords( pnt )
+        self.srcNode = self.HitTest( winPnt )
+        if self.srcNode is not None:
+            self.srcPlug = self.srcNode.HitTest( winPnt )
             if self.srcPlug is not None:
-                for dstNode in self.nodes.values():
-                    dstPlug = dstNode.HitTest( winSpacePos )
-                    if dstPlug is not None:
-                        #pnt1 = (self.srcPlug.pos[0] + self.srcNode.pos[0], self.srcPlug.pos[1] + self.srcNode.pos[1])
-                        #pnt2 = (dstPlug.pos[0] + dstNode.pos[0], dstPlug.pos[1] + dstNode.pos[1])
-                        #self.DrawWire( pnt1, pnt2, self.pdc)
-                        #print 'CONNECT:', self.srcPlug, dstPlug
-                        wire = Wire( self.srcNode, self.srcPlug, dstNode, dstPlug )
-                        self.srcNode.wires.append( wire )
-                        self.DoDrawing( self.pdc )
+                self.tmpWire = Wire( self.srcNode.pos + self.srcPlug.pos, pnt )
+        self.lastPnt = pnt
 
-            self._startPos = self._pos = None
-            self.srcPlug = None
+    def OnMotion( self, evt ):
+        if not evt.LeftIsDown() or self.srcNode is None:
+            return
 
-        elif event.Dragging():
-            self._pos = x, y
+        pnt = evt.GetPosition()
+        winPnt = self.ConvertCoords( pnt )
+        if self.srcPlug is None:
+            dPnt = pnt - self.lastPnt
+            r = self.pdc.GetIdBounds( self.srcNode._idx )
+            self.pdc.TranslateId( self.srcNode._idx, dPnt[0], dPnt[1] )
+            r2 = self.pdc.GetIdBounds( self.srcNode._idx )
+            r = r.Union( r2 )
+            self.OffsetRect( r )
+            self.RefreshRect( r, False )
+            self.lastPnt = pnt
+            self.srcNode.UpdateRect( r2 )
 
-            # HAXXOR
-            r = wx.Rect( 0, 0, 400, 400 )
-            self.RefreshRect(r, True)
+            # Redraw wires
+            for wire in self.srcNode.wires:
+                pnt1 = wire.srcNode.pos + wire.srcPlug.pos
+                pnt2 = wire.dstNode.pos + wire.dstPlug.pos
+                self.DrawWire( wire, pnt1, pnt2 )
 
+        elif self.tmpWire is not None:
+            self.DrawWire( self.tmpWire, pnt2=winPnt )
+
+    def OnLeftUp( self, evt ):
+
+        # Attempt to make a connection.
+        if self.srcNode is not None:
+            pnt = evt.GetPosition()
+            winPnt = self.ConvertCoords( pnt )
+            dstNode = self.HitTest( winPnt )
+            if dstNode is not None:
+                dstPlug = dstNode.HitTest( winPnt )
+                if dstPlug is not None:
+                    self.ConnectNodes( self.srcNode, self.srcPlug, dstNode, dstPlug )
+
+        # Erase the temp wire.
+        if self.tmpWire is not None:
+            rect = self.pdc.GetIdBounds( self.tmpWire._idx )
+            self.pdc.RemoveId( self.tmpWire._idx ) 
+            self.OffsetRect( rect )
+            self.RefreshRect( rect, False )
+
+        self.srcNode = None
+        self.srcPlug = None
+        self.tmpWire = None
+
+    def HitTest( self, pt ):
+        idxs = self.pdc.FindObjects( pt[0], pt[1], HIT_RADIUS )
+        hits = [
+            idx 
+            for idx in idxs
+            if idx in self.nodes
+        ]
+        return self.nodes[hits[0]] if hits else None
 
     def OnPaint(self, event):
-        
-        # Create a buffered paint DC.  It will create the real
-        # wx.PaintDC and then blit the bitmap to it when dc is
-        # deleted.  
+
+        # Create a buffered paint DC.  It will create the real wx.PaintDC and 
+        # then blit the bitmap to it when dc is deleted.
         dc = wx.BufferedPaintDC( self )
-        # use PrepateDC to set position correctly
+        dc = wx.GCDC( dc )
+
+        # Use PrepateDC to set position correctly.
         self.PrepareDC( dc )
-        # we need to clear the dc BEFORE calling PrepareDC
+
+        # We need to clear the dc BEFORE calling PrepareDC.
         bg = wx.Brush( self.GetBackgroundColour() )
         dc.SetBackground( bg )
         dc.Clear()
-        # create a clipping rect from our position and size
-        # and the Update Region
+
+        # Create a clipping rect from our position and size and the Update 
+        # Region.
         xv, yv = self.GetViewStart()
         dx, dy = self.GetScrollPixelsPerUnit()
         x, y = (xv * dx, yv * dy)
@@ -240,47 +283,39 @@ class NodeGraph( wx.ScrolledWindow ):
         rgn.Offset( x, y )
         r = rgn.GetBox()
 
-        # Draw the temp wire.
-        if self._startPos is not None and self._pos is not None:
-            self.DrawWire( self._startPos, self._pos, dc )
-
         # Draw to the dc using the calculated clipping rect.
         self.pdc.DrawToDCClipped( dc, r )
 
-    def DrawWire( self, pos1, pos2, dc ):
-        pnts = []
-        pnts.append( pos1 )
-        pnts.append( (pos1[0] + 20, pos1[1]) )
-        pnts.append( (pos2[0] - 20, pos2[1]) )
-        pnts.append( pos2 )
-        dc.DrawSpline( pnts )
+    def ConnectNodes( self, srcNode, srcPlug, dstNode, dstPlug ):
+        wire = Wire( srcNode.pos + srcPlug.pos, dstNode.pos + dstPlug.pos )
+        wire.srcNode = srcNode
+        wire.dstNode = dstNode
+        wire.srcPlug = srcPlug
+        wire.dstPlug = dstPlug
+        srcNode.wires.append( wire )
+        dstNode.wires.append( wire )
 
-    def DoDrawing( self, dc ):
-        print 'Do Drawing'
-        #dc.Clear()
-        dc.BeginDrawing()
-        
-        for node in self.nodes.values():
-            #if node.drawn:
-            #    continue
-            dc.SetId( node._idx )
-            node.Draw( dc )
-            r = wx.Rect( node.pos[0], node.pos[1], node.size[0], node.size[1] )
-            dc.SetIdBounds( node._idx, r )
-            #node.drawn = True
+        for wire in srcNode.wires:
+            self.pdc.SetId( wire._idx )
+            wire.Draw( self.pdc )
+            self.pdc.SetIdBounds( wire._idx, wire.GetRect() )
+            self.RefreshRect( wire.GetRect(), False )
 
-            # Draw node wires.
-            for wire in node.wires:
+    def DrawWire( self, wire, pnt1=None, pnt2=None ):
+        rect1 = wire.GetRect()
+        if pnt1 is not None:
+            wire.pnt1 = pnt1
+        if pnt2 is not None:
+            wire.pnt2 = pnt2
+        rect2 = wire.GetRect()
+        rect = rect1.Union( rect2 )
+        self.OffsetRect( rect )
 
-                dc.SetId( wire._idx )
-                wire.Draw( dc )
-                pnt1 = wire.node1.pos + wire.plug1.pos
-                pnt2 = wire.node2.pos + wire.plug2.pos
-                r = wx.Rect( pnt1.x, pnt1.y, pnt2.x - pnt1.x, pnt2.y - pnt1.y )
-                dc.SetIdBounds( wire._idx, r )
-                #wire.Draw()
-
-        dc.EndDrawing()
+        self.pdc.ClearId( wire._idx )
+        self.pdc.SetId( wire._idx )
+        wire.Draw( self.pdc )
+        self.pdc.SetIdBounds( wire._idx, wire.GetRect() )
+        self.RefreshRect( rect, False )
 
 
 class MainFrame( wx.Frame ):
@@ -290,8 +325,6 @@ class MainFrame( wx.Frame ):
 
         pnl = wx.Panel( self, wx.ID_ANY, size=(200,30) )
         ng = NodeGraph( pnl, wx.ID_ANY, None )
-        #ng.AddNode( 'foo', (66, 66), [], ['a', 'b', 'c'] )
-        #ng.AddNode( 'bar', (200, 10), ['a', 'b', 'c'], [] )
         ng.AddNode( '1', (66, 66), [], ['a'] )
         ng.AddNode( '2', (66, 200), [], ['a'] )
         ng.AddNode( 'Add', (200, 80), ['a', 'b'], [] )
@@ -302,7 +335,7 @@ class MainFrame( wx.Frame ):
 
 if __name__ == '__main__':
     app = wx.App( redirect=False )
-    top = MainFrame( None, title='foo', pos=(0, 0), size=(700, 600) )
+    top = MainFrame( None, title='foo', pos=(0, 0), size=(500, 600) )
     top.Show()
     app.SetTopWindow( top )
     app.MainLoop()
